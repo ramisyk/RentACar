bd2f3b194c252c9ff3f601ee6358be75314e8410
Ramis Yuksel <ramisyk97@gmail.com>
2021-02-14 19:27:35 +0300

    car object is created

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //Classes that implemented this interface are entity -so database object table or joined table-
+    public interface IEntity
+    {
+
+    }
+}
