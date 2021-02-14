841a2be8b6b53b35f229110fbc15e7b41f99268b
Ramis Yuksel <ramisyk97@gmail.com>
2021-02-14 19:56:45 +0300

    list cars tested

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -7,7 +7,7 @@ using System.Text;
 
 namespace Business.Concrete
 {
-    class CarManager : ICarService
+    public class CarManager : ICarService
     {
         ICarDal _carDal;
         public CarManager(ICarDal carDal)
