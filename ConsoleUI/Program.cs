e6c781c445de5764d97f582b03bae119f0214f0f
Ramis Yuksel <ramisyk97@gmail.com>
2021-02-16 14:34:06 +0300

    ..

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ListCarsTest();
+            //ListCarsTest();
             //AddCarTest();
             //ListBrandTest();
             //ListColorTest();
