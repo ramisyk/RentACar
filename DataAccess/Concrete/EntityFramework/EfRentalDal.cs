e158521ac82c50b27599465576aed2c384df96d9
Ramis Yuksel <ramisyk97@gmail.com>
2021-02-21 17:11:47 +0300

    ..

--- DataAccess/Concrete/EntityFramework/EfRentalDal.cs
+++ DataAccess/Concrete/EntityFramework/EfRentalDal.cs
@@ -1,6 +1,8 @@
 ﻿using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
+using System.Linq;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,5 +11,31 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfRentalDal : EfEntityRepositoryBase<Rental, CarsDbContext>, IRentalDal
     {
+        public List<RentalDetailDto> GetAllRentalDetail()
+        {
+            using (CarsDbContext context = new CarsDbContext())
+            {
+                var result = from rental in context.Rentals
+                             join car in context.Cars
+                             on rental.CarId equals car.CarId
+                             join customer in context.Customers
+                             on rental.CustomerId equals customer.CustomerId
+                             join user in context.Users
+                             on customer.UserId equals user.UserId
+                             select new RentalDetailDto
+                             {
+                                 FirstName = user.FirstName,
+                                 LastName = user.LastName,
+                                 CarName = car.Description,
+                                 CarId = car.CarId,
+                                 DailyPrice = car.DailyPrice,
+                                 ReturnDate = rental.ReturnDate,
+                                 RentDate = rental.RentDate
+                             };
+
+                return result.ToList();
+            }
+
+        }
     }
 }
--- Entities/DTOs/RentalDetailDto.cs
+++ Entities/DTOs/RentalDetailDto.cs
@@ -6,8 +6,12 @@ namespace Entities.DTOs
 {
     public class RentalDetailDto
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CarId { get; set; }
         public string CarName { get; set; }
+        public decimal DailyPrice { get; set; }
         public DateTime RentDate { get; set; }
-        public DateTime? ReturnDate { get; set; }
+        public DateTime ReturnDate { get; set; }
     }
 }
