49608c640e119d13e858d4bd3daca70e0cb7eaea
Ramis Yuksel <ramisyk97@gmail.com>
2021-02-21 15:16:03 +0300

    cars controller created and dependency is solved

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +15,21 @@ namespace WebAPI.Controllers
     [ApiController]
     public class CarsController : ControllerBase
     {
+        //dependency on abstract so loosely coupled
+        //add singleton for not new() for service and manager
+        ICarService _carService;
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+        
+        [HttpGet]
+        public List<Car> Get()
+        {
+            //Dependency chain 
+            //ICarService carService = new CarManager(new EfCarDal());
+            var result = _carService.GetAll();
+            return result.Data;
+        }
     }
 }
