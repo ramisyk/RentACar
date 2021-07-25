cb5c873234b85112d0970c1ca96ec994b0a4ac33
ramisyk <ramisyk@RAMISYK>
2021-07-25 19:04:44 +0300

    update images codes

---
+++ WebAPI/Controllers/ImagesController.cs
@@ -0,0 +1,68 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        IImageService _imageService;
+
+        public ImagesController(IImageService imageService)
+        {
+            _imageService = imageService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Dependency chain 
+            //ICarService carService = new CarManager(new EfCarDal());
+            var result = _imageService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        
+        [HttpPost("add")]
+        public IActionResult Add(Image image)
+        {
+            var result = _imageService.Add(image);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpDelete("delete")]
+        public IActionResult Delete(Image image)
+        {
+            var result = _imageService.Delete(image);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPut("update")]
+        public IActionResult Update(Image image)
+        {
+            var result = _imageService.Update(image);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
