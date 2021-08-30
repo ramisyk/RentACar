8f8bb2e6fa6fd9a865624184f7392532bf66c13e
ramisyk <ramisyk@RAMISYK>
2021-08-30 18:41:07 +0300

    user clains and jwt settings added

---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
